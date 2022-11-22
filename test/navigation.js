462e7c80803b5a06ba3a1cf9dc048a485b0017ed
andreishevelev <93206992+andreishevelev@users.noreply.github.com>
2022-11-22 09:59:11 -0600

    Update navigation.js

--- test/navigation.js
+++ test/navigation.js
@@ -115,5 +115,6 @@ describe('Can check navigation on Dice.com', function () {
       //actionStr += `${actions[l].questionType} (${JSON.stringify(actions[l].answer)})`
     }
     console.log(actionStr);
+    console.log(`jenkins integration`);
   }
 });
